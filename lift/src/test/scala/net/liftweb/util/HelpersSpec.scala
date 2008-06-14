36a45e51cb9a64af529b9ef113f32de392a42c01
Jorge Ortiz <jeortiz@jorge-ortizs-macbook-pro.local>
2008-06-14 14:11:24 -0700

    Adding some tests for Helpers

--- lift/src/test/scala/net/liftweb/util/HelpersSpec.scala
+++ lift/src/test/scala/net/liftweb/util/HelpersSpec.scala
@@ -1,7 +1,16 @@
 package net.liftweb.util
+
 import org.specs.Specification
 import org.specs.runner._
+
+import Helpers._
+
 class HelpersTest extends Runner(HelpersSpec) with JUnit
 
 object HelpersSpec extends Specification {
+  "paramsToUrlParams" should {
+    "handle the empty case" in {
+      paramsToUrlParams(Nil) mustBe ""
+    }
+  }
 }
