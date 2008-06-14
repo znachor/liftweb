7ba1a1b8098f79f13116c9672790322afc76cecf
Jorge Ortiz <jeortiz@jorge-ortizs-macbook-pro.local>
2008-06-14 15:33:35 -0700

    More Helpers specs

--- lift/src/test/scala/net/liftweb/util/HelpersSpec.scala
+++ lift/src/test/scala/net/liftweb/util/HelpersSpec.scala
@@ -9,8 +9,28 @@ class HelpersTest extends Runner(HelpersSpec) with JUnit
 
 object HelpersSpec extends Specification {
   "paramsToUrlParams" should {
-    "handle the empty case" in {
-      paramsToUrlParams(Nil) mustBe ""
+    "handle no parameters" in {
+      paramsToUrlParams(Nil) mustEqual ""
+    }
+    
+    "handle one parameter" in {
+      paramsToUrlParams(List(("foo", "bar"))) mustEqual "foo=bar"
+    }
+    
+    "handle two parameters" in {
+      paramsToUrlParams(List(
+        ("foo", "bar"), ("fooz", "baz")
+      )) mustEqual "foo=bar&fooz=baz"
+    }
+    
+    "handle many parameters" in {
+      paramsToUrlParams(List(
+        ("foo", "bar"), ("fooz", "baz"), ("fooy", "bay"), ("foox", "bax")
+      )) mustEqual "foo=bar&fooz=baz&fooy=bay&foox=bax"
+    }
+    
+    "handle collections" in {
+      paramsToUrlParams(Map("foo" -> "bar")) mustEqual "foo=bar"
     }
   }
 }
