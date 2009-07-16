1b296876505b3d94e23dad7841e6fdc67aa3d11e
Timothy Perrett <hello@timperrett.com>
2009-07-16 20:11:14 +0100

    debugging upload progess widget

--- lift-widgets/src/main/scala/net/liftweb/widgets/uploadprogress/UploadProgress.scala
+++ lift-widgets/src/main/scala/net/liftweb/widgets/uploadprogress/UploadProgress.scala
@@ -5,7 +5,7 @@ import _root_.net.liftweb.http.{SessionVar,Req,GetRequest,PlainTextResponse,Json
                                 LiftRules,OnDiskFileParamHolder,S,ResourceServer}
 import _root_.net.liftweb.http.js.JsCmds._
 import _root_.net.liftweb.http.js.JE._
-import _root_.net.liftweb.util.{Box,Empty,Failure,Full}
+import _root_.net.liftweb.util.{Box,Empty,Failure,Full,Log}
 
 import _root_.scala.actors.Actor
 import _root_.scala.actors.Actor._
@@ -33,22 +33,24 @@ object UploadProgress {
     LiftRules.maxMimeFileSize = LiftRules.maxMimeSize
     
     ResourceServer.allow({
-      case "uploadprogress" :: "uploadprogress.js" :: Nil => true
+      case "uploadprogress" :: "jquery.uploadprogress.0.3.js" :: Nil => true
       case "uploadprogress" :: "jquery.timers-1.1.2.js" :: Nil => true
     })
     
     LiftRules.dispatch.append {
       case Req("progress" :: Nil, "", GetRequest) => () => {
-        var recived: Double = StatusHolder.is.map(_._1.toDouble).openOr(0D)
-        var size: Double = StatusHolder.is.map(_._2.toDouble).openOr(0D)
+        //var recived: Double = StatusHolder.is.map(_._1.toDouble).openOr(0D)
+        //var size: Double = StatusHolder.is.map(_._2.toDouble).openOr(0D)
         
         println(StatusHolder.is)
-        println(recived)
-        println(size)
+        //println(recived)
+        //println(size)
+        //Str(Math.floor((recived.toDouble / size.toDouble)*100).toString
+        Log.info("DISPATCHING UPLOAD PROGRESS")
         
         Full(JsonResponse(
           JsObj("state" -> "uploading", 
-                "percentage" -> Str(Math.floor((recived.toDouble / size.toDouble)*100).toString)
+                "percentage" -> "1234"
           ))
         )
       }
@@ -56,24 +58,13 @@ object UploadProgress {
     
   }
   
-  // private object StautsActor extends Actor {
-  //   def act() {
-  //     loop {
-  //       react {
-  //         case StatusUpdate(read,total) => StatusHolder(Full((read, total)))
-  //       }
-  //     }
-  //   }
-  // }
-  
-  // private case class StatusUpdate(bytesRead: Long, bytesTotal: Long)
-  
   def sessionProgessListener =
     S.session.foreach(s => { 
       s.progessListener = Full((pBytesRead: Long, pBytesTotal: Long, pItem: Int) => {
-        //StautsActor ! StatusUpdate(pBytesRead,pBytesTotal)
+        // println()
         StatusHolder(Full((pBytesRead, pBytesTotal)))
-        //println(StatusHolder.is)
+        println(StatusHolder.is)
+        Thread.sleep(500)
       })
     })
   
