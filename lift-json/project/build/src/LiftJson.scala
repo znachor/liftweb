5b16f3cfc57050f27b372fa50ba30fd9dde665f8
Joni Freeman <joni.freeman@reaktor.fi>
2009-08-12 15:51:24 +0300

    Add junit as a dependency.

--- lift-json/project/build/src/LiftJson.scala
+++ lift-json/project/build/src/LiftJson.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class LiftJson(info: ProjectInfo) extends DefaultProject(info) {
+  val junit      = "junit" % "junit" % "4.5"
   val specs      = "org.scala-tools.testing" % "specs" % "1.5.0"
   val scalacheck = "org.scala-tools.testing" % "scalacheck" % "1.5"
   
