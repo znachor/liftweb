e775a1e4a06325b8ac66fdbd59b29f904e5459a8
David Pollak <feeder.of.the.bears@gmail.com>
2009-09-10 16:56:17 -0700

    Reverted to DI style

--- lift-archetypes/lift-archetype-basic/src/main/resources/archetype-resources/src/main/scala/snippet/HelloWorld.scala
+++ lift-archetypes/lift-archetype-basic/src/main/resources/archetype-resources/src/main/scala/snippet/HelloWorld.scala
@@ -7,17 +7,16 @@ import ${packageName}.lib._
 import Helpers._
 
 class HelloWorld {
+  lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+
+  def howdy(in: NodeSeq): NodeSeq =
+  Helpers.bind("b", in, "time" -> date.map(d => Text(d.toString)))
+   
   /*
-   lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
+   lazy val date: Date = DependencyFactory.time.vend // create the date via factory
 
-   def howdy(in: NodeSeq): NodeSeq =
-   Helpers.bind("b", in, "time" -> date.map(d => Text(d.toString)))
+   def howdy(in: NodeSeq): NodeSeq = Helpers.bind("b", in, "time" -> date.toString)
    */
-  
-  lazy val date: Date = DependencyFactory.time.vend // create the date via factory
-
-  def howdy(in: NodeSeq): NodeSeq = Helpers.bind("b", in, "time" -> date.toString)
-  
 }
 
 
