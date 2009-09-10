4eab60fbae3fa1fb2ca9fec4d75f91caf37bf17f
David Pollak <feeder.of.the.bears@gmail.com>
2009-09-10 16:18:37 -0700

    Improved test template

--- lift-archetypes/lift-archetype-basic/src/main/resources/archetype-resources/src/main/scala/snippet/HelloWorld.scala
+++ lift-archetypes/lift-archetype-basic/src/main/resources/archetype-resources/src/main/scala/snippet/HelloWorld.scala
@@ -3,15 +3,21 @@ package ${packageName}.snippet
 import _root_.scala.xml.{NodeSeq, Text}
 import _root_.net.liftweb.util._
 import _root_.java.util.Date
-import Helpers._
 import ${packageName}.lib._
+import Helpers._
 
 class HelloWorld {
-  lazy val date = DependencyFactory.inject[Date] // inject the date 
-  /* lazy val date = DependencyFactory.time.make // create the date via factory */
+  /*
+   lazy val date: Box[Date] = DependencyFactory.inject[Date] // inject the date
 
-  def howdy(in: NodeSeq): NodeSeq =
-    Helpers.bind("b", in, "time" -> date.map(d => Text(d.toString)))
+   def howdy(in: NodeSeq): NodeSeq =
+   Helpers.bind("b", in, "time" -> date.map(d => Text(d.toString)))
+   */
+  
+  lazy val date: Date = DependencyFactory.time.vend // create the date via factory
+
+  def howdy(in: NodeSeq): NodeSeq = Helpers.bind("b", in, "time" -> date.toString)
+  
 }
 
 
